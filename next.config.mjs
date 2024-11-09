39178229b5a134713c3a95acf13bcc31427c67a1
ikifar2012 <ikifar2012@users.noreply.github.com>
2024-11-09 02:25:43 +0000

    Update Next.js configuration for image domains, add new dependency for icon pack, and upgrade package versions

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
         dynamicIO: true,
     },
     images: {
-        domains: ['is1-ssl.mzstatic.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'is1-ssl.mzstatic.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     }
 };
 
