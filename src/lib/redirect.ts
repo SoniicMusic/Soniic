c0a070587e46d1cf35b55afb8f7902fce20032d7
Matheson Steplock <ikifar2012@users.noreply.github.com>
2025-05-07 00:15:34 +0000

    ✨ Implement dynamic redirect functionality based on environment

--- src/lib/redirect.ts
+++ src/lib/redirect.ts
@@ -2,16 +2,18 @@ import { redirect } from "next/navigation";
 
 const NODE_ENV = process.env.NODE_ENV;
 
-export function dynamicRedirect(url) {
+export function dynamicRedirect(url: string) {
   if (NODE_ENV === 'production') {
     redirect(`https://${url}`);
-  }
-  if (NODE_ENV === 'development') {
+  } else if (NODE_ENV === 'development') {
     // Redirect in development
     // get domain from beginning of url and add port before the rest of the url
     const domain = url.split('/')[0];
     const path = url.split('/').slice(1).join('/');
     redirect(`http://${domain}:3000/${path}`);
+  } else {
+
+    console.warn('No redirect for this environment');
   }
 
   }
